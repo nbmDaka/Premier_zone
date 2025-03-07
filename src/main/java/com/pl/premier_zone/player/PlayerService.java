fc7b87a7400a7d751013c14df1ebe5d49fa694cd
nbmdaka <nurdaulet.beketov.2005@gmail.com>
2025-03-07 15:51:47 +0500

    Finishing Project(YouTube video)

--- src/main/java/com/pl/premier_zone/player/PlayerService.java
+++ src/main/java/com/pl/premier_zone/player/PlayerService.java
@@ -1,9 +1,11 @@
 package com.pl.premier_zone.player;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -46,8 +48,34 @@ public class PlayerService {
 
     public List<Player> getPlayersByTeamAndPosition(String team, String position) {
         return playerRepository.findAll().stream()
-                .filter(player -> (player.getTeam().equals(team) && player.getPos().equals(position)))
+                .filter(player -> team.equals(player.getTeam()) && position.equals(player.getPos()))
                 .collect(Collectors.toList());
     }
 
+    public Player addPlayer(Player player) {
+        playerRepository.save(player);
+        return player;
+    }
+
+    public Player updatePlayer(Player updatedPlayer) {
+        Optional<Player> existingPlayer = playerRepository.findByName(updatedPlayer.getName());
+
+        if(existingPlayer.isPresent()) {
+            Player playerToUpdate = existingPlayer.get();
+            playerToUpdate.setName(updatedPlayer.getName());
+            playerToUpdate.setTeam(updatedPlayer.getTeam());
+            playerToUpdate.setPos(updatedPlayer.getPos());
+            playerToUpdate.setNation(updatedPlayer.getNation());
+
+            playerRepository.save(playerToUpdate);
+            return playerToUpdate;
+        }
+
+        return null;
+    }
+
+    @Transactional
+    public void deletePlayer(String playerName) {
+        playerRepository.deleteByName(playerName);
+    }
 }
